b2bdfa603ffb4caa7d471f0fc7a38f6b71bece4e
Pro-Developer01 <yadavshashank70@gmail.com>
2022-09-23 01:40:16 +0530

    modal logic and map in allnotes page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AllNotes from '../Pages/AllNotes.js'
 import CreateNote from '../Pages/CreateNote.js'
 
 export default function Dashboard() {
-  return (
-    <div>
-      <div className="btn-group">
-  <a href="/" className="btn btn-outline-primary " aria-current="page">Active link</a>
-  <a href="/" className="btn btn-outline-primary">Link</a>
-  <a href="/" className="btn btn-outline-primary">Link</a>
-</div>
-<div className="results">
-<CreateNote/>
-<AllNotes/>
-</div>
-    </div>
-  )
+    const [CreateNte, setCreateNte] = useState(true);
+    const [AllNote, setAllNote] = useState(false);
+    return (
+        <div className='container d-flex-column justify-content-center'>
+            <div className="btn-group d-flex justify-content-center my-4" style={{width: '100%'}}>
+                <div>
+
+                <button className="btn btn-outline-primary mx-3" onClick={() => { setCreateNte(CreateNte ? false : true); setAllNote(false) }}>CreateNote</button>
+                <button className="btn btn-outline-primary mx-3" onClick={() => { setAllNote(AllNote ? false : true); setCreateNte(false) }} >AllNotes</button>
+                </div>
+            </div>
+            <hr />
+            <div className="results d-flex justify-content-center">
+                {CreateNte && <CreateNote />}
+                {AllNote && <AllNotes />}
+            </div>
+        </div>
+    )
 }
--- src/components/Pages/AllNotes.js
+++ src/components/Pages/AllNotes.js
@@ -1,34 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+const data=[
+    {name:'Binod Saxena' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:' Saxena' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:'Binod ' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:' Saxena' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:'hello ' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:' Saxena' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:'hello ' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:' Saxena' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+    {name:'hello ' , para:"jbdcjsd sdbdjvbjsdv djvbsdhvb jvbjvb "},
+]
 
 export default function AllNotes() {
-  return (
-    <div>
-      <div className="list-group">
-  <a href="/" className="list-group-item list-group-item-action active" aria-current="true">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">List group item heading</h5>
-      <small>3 days ago</small>
-    </div>
-    <p className="mb-1">Some placeholder content in a paragraph.</p>
-    <small>And some small print.</small>
-  </a>
-  <a href="/" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">List group item heading</h5>
-      <small className="text-muted">3 days ago</small>
-    </div>
-    <p className="mb-1">Some placeholder content in a paragraph.</p>
-    <small className="text-muted">And some muted small print.</small>
-  </a>
-  <a href="/" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">List group item heading</h5>
-      <small className="text-muted">3 days ago</small>
-    </div>
-    <p className="mb-1">Some placeholder content in a paragraph.</p>
-    <small className="text-muted">And some muted small print.</small>
-  </a>
-</div>
-    </div>
-  )
+    const[Index,setIndex]=useState();
+    console.log('index',Index);
+    return (
+        <div style={{
+            height: '74vh',
+            background: '#f9f0f0bd',
+            overflow: 'auto'
+
+        }}>
+            <div className="list-group">
+                {data.map((e)=>{
+                    return(
+                        <>
+                        <button href="/" className="list-group-item list-group-item-action " data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>setIndex(e)} aria-current="true">
+                <div className="d-flex w-100 justify-content-between" >
+                        <h5 className="mb-1">{e.name}</h5>
+                        {/* <small>3 days ago</small> */}
+                    </div>
+                    <p className="mb-1">{e.para}</p>
+                    {/* <small>And some small print.</small> */}
+                </button>
+                        </>
+                    );
+                })}
+                
+               
+            </div>
+
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">{Index?Index.name:null}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                        {Index?Index.para:null}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Update Note</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
